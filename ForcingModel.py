b995b7061a242ebe58ebc0ce616f7c7b1158e123
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2021-05-07 10:24:06 -0600

    add the day len calculation

--- ForcingModel.py
+++ ForcingModel.py
@@ -4,11 +4,43 @@ import pandas as pd
 import metpy.calc as mpcalc
 from metpy.units import units
 import sys
-from lumped_hydro_model import *
 import xarray as xr
 from scipy import optimize
 
 # parameters
+def DayLength_Calc(dayOfYear, lat=40.0):
+    """Computes the length of the day (the time between sunrise and
+    sunset) given the day of the year and latitude of the location.
+    Function uses the Brock model for the computations.
+    For more information see, for example,
+    Forsythe et al., "A model comparison for daylength as a
+    function of latitude and day of year", Ecological Modelling,
+    1995.
+    Parameters
+    ----------
+    dayOfYear : int
+        The day of the year. 1 corresponds to 1st of January
+        and 365 to 31st December (on a non-leap year).
+    lat : float
+        Latitude of the location in degrees. Positive values
+        for north and negative for south.
+    Returns
+    -------
+    d : float
+        Daylength in hours.
+    """
+    latInRad = np.deg2rad(lat)
+    declinationOfEarth = 23.45*np.sin(np.deg2rad(360.0*(283.0+dayOfYear)/365.0))
+    if -np.tan(latInRad) * np.tan(np.deg2rad(declinationOfEarth)) <= -1.0:
+        return 24.0
+    elif -np.tan(latInRad) * np.tan(np.deg2rad(declinationOfEarth)) >= 1.0:
+        return 0.0
+    else:
+        hourAngle = np.rad2deg(np.arccos(-np.tan(latInRad) * np.tan(np.deg2rad(declinationOfEarth))))
+        return 2.0*hourAngle/15.0
+
+    # https://gist.github.com/anttilipp/ed3ab35258c7636d87de6499475301ce
+
 
 class ForcingModel:
     def __init__(self):
