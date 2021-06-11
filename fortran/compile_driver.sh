15f9d632d7b8a22f67655669ef2fc0b65ddcaefd
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2021-06-11 14:27:47 -0600

    add WORKING hymod

--- fortran/compile_driver.sh
+++ fortran/compile_driver.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#gfortran -c snowmodule17.f90
+#gfortran -c driver.f90
+#f2py -c snowmodule17.f90 driver.f90 -m driver
+
 gfortran -c snowmodule17.f90
-gfortran -c driver.f90
-f2py -c snowmodule17.f90 driver.f90 -m driver
+gfortran -c Hymod.f90 
+f2py -c snowmodule17.f90 hymod.f90 -m hymod
